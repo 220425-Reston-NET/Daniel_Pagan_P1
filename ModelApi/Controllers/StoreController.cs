f0469bd98b26fc927ff9300474be2ad31d407aff
Vyzoroy <dpagan7@gmail.com>
2022-06-07 23:17:00 -0400

    edited controller

--- ModelApi/Controllers/StoreController.cs
+++ ModelApi/Controllers/StoreController.cs
@@ -3,22 +3,22 @@ using Microsoft.Data.SqlClient;
 using ModelBL;
 using ModelGundam;
 
-// namespace ModelApi.Controllers
-// {
-//     [ApiController]
-//     [Route("api/[controller")]
-//     public class StoreController : ControllerBase
-//     {
-//         private ICustomerBL _storeBL;
-//         public StoreController(IStoreBL m_storeBL)
-//         {
-//             _storeBL = m_storeBL;
-//         }
-//         [HttpGet("ViewStoreInventory")]
-//         public IActionResult ViewStoreInventory([FromQuery] int m_sID)
-//         {
-//             return Ok(_storeBL.ViewStoreInventory(m_sID));
-//         }
+namespace ModelApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller")]
+    public class StoreController : ControllerBase
+    {
+        private IStoreBL _storeBL;
+        public StoreController(IStoreBL m_storeBL)
+        {
+            _storeBL = m_storeBL;
+        }
+        [HttpGet("ViewStoreInventory")]
+        public IActionResult ViewStoreInventory([FromQuery] int m_sID)
+        {
+            return Ok(_storeBL.ViewStoreInventory(m_sID));
+        }
         
-//     }
-// }
+    }
+}
